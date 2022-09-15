d6d3edb5b74058b71cefed5887c5fc225888e150
thegauravonline <thegauravonline@gmail.com>
2022-09-15 18:55:30 +0530

    Update connect.php

--- Admin/include/connect.php
+++ Admin/include/connect.php
@@ -1,5 +1,5 @@
 
 <?php
-$conn=mysqli_connect("u766367794_light","Light@321","u143481292_newgopali_123");
+$conn=mysqli_connect("localhost","u766367794_light","Light@321","u143481292_newgopali_123");
 ?>
 
